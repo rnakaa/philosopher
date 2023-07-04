374e68c47e846041bc8a22e68d2614c09bad7aa3
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-07-04 22:48:57 +0900

    README

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void *do_something(void *arg)
+void *philo(void *arg)
 {
-   printf("do something\n");
+	while (1)
+		if (i == 3)
+			break ;
+	printf("haihaihai\n");
+}
+
+void *gen_philo(void *arg)
+{
+   int	i;
+   pthread_t philo[3];
+   
+   i = 0;
+   while (i < 3)
+   {
+	if (pthread_create(&philo[i], NULL, philo, NULL) != 0)
+		printf("error\n");
+   	i++;
+   }
    sleep(1);
 }
 
@@ -14,15 +31,13 @@ int main(void)
    pthread_t thread;
    void *retval;
 
-   // スレッドを作成
-   if (pthread_create(&thread, NULL, do_something, NULL) != 0)
+   if (pthread_create(&thread, NULL, gen_philo, NULL) != 0)
        // エラー処理
        return (0);
-   // スレッドの終了方法
    // 1. スレッドが終了するまで待つ
    pthread_join(thread, &retval);
    // 2. スレッドを待たない
-   pthread_detach(thread);
+   // pthread_detach(thread);
    return (0);
 }
 
